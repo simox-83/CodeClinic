5ae0ac035501d9c0572f3afddec7e0951570ae2a
simoned <simone.dandreta@concur.com>
2019-01-18 12:14:00 +0100

    adding test using table struct and assert library

--- 01-parsingdata/main_test.go
+++ 01-parsingdata/main_test.go
@@ -1,21 +1,47 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestMean(t *testing.T) {
-	input := [][]string{
-		[]string{"", "3", "3", "", "", "", "", "6"},
-		[]string{"", "3", "3", "", "", "", "", "6"},
+	table := []struct {
+		input            [][]string
+		out1, out2, out3 float64
+	}{
+		{
+			[][]string{
+				[]string{"", "1", "1", "", "", "", "", "1"},
+				[]string{"", "3", "3", "", "", "", "", "6"},
+				[]string{"", "7", "5", "", "", "", "", "9"},
+			},
+			5.0, 4.0, 7.5,
+		},
 	}
-	exp1, exp2, exp3 := mean(input)
+	/*
+		for _, el := range table {
+			expected1, expected2, expected3 := mean(el.input)
+			if expected1 != el.out1 {
+				t.Errorf("expected1 got %f, expected %f", el.out1, 5.000000)
+			}
+			if expected2 != el.out2 {
+				t.Errorf("expected2 got %f, expected %f", el.out2, 4.000000)
+			}
+			if expected3 != el.out3 {
+				t.Errorf("expected3 got %f, expected %f", el.out3, 7.500000)
+			}
+		}
+	*/
 
-	if exp1 != 3.0 {
-		t.Errorf("exp1 got %f, expected %f", exp1, 3.0)
+	// 2. usare github.com/stretchr/testify/assert al post di t.Errorf
+	for _, el := range table {
+		res1, res2, res3 := mean(el.input)
+		assert.Equal(t, res1, el.out1, "Mean Temperature is wrong")
+		assert.Equal(t, res2, el.out2, "Mean Pressure is wrong")
+		assert.Equal(t, res3, el.out3, "Mean Wind is wrong")
 	}
-	if exp2 != 3.0 {
-		t.Errorf("exp2 got %f, expected %f", exp1, 3.0)
-	}
-	if exp3 != 6.0 {
-		t.Errorf("exp3 got %f, expected %f", exp1, 6.0)
-	}
+
+	// 3. aggiungere piu' test
 }
